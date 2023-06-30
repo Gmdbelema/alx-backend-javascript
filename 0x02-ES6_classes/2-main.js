f94914ac91ca8bb38070ff3d13b394d18dae708f
Belema Acheye <bjhoodbelema@gmail.com>
2023-06-30 22:07:12 +0000

    0-100-main.js

--- 0x02-ES6_classes/2-main.js
+++ 0x02-ES6_classes/2-main.js
@@ -1,9 +1,9 @@
-import HolbertonCourse from "./2-hbtn_course";
+import HolbertonCourse from './2-hbtn_course';
 
 try {
-  const c1 = new HolbertonCourse("ES6", 1, ["Bob", "Jane"]);
+  const c1 = new HolbertonCourse('ES6', 1, ['Bob', 'Jane']);
   console.log(c1.name);
-  c1.name = "Python 101";
+  c1.name = 'Python 101';
   console.log(c1);
 
   c1.name = 12;
@@ -12,8 +12,7 @@ try {
 }
 
 try {
-  const c2 = new HolbertonCourse("ES6", "1", ["Bob", "Jane"]);
+  // const c2 = new HolbertonCourse('ES6', '1', ['Bob', 'Jane']);
 } catch (err) {
   console.error(err.message);
 }
-
